6f9dee3267b0e320c06f5d144ff12e7a2e05495d
Nate Dean <nate.dean@pathar.net>
2017-01-28 17:48:38 -0600

    About halfway through the redux-flow module

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,10 +1,41 @@
 import React, { PropTypes } from 'react';
 
 class CoursesPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      course: { title: '' }
+    }
+
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChange(e) {
+    const course = this.state.course;
+    course.title = e.target.value;
+    this.setState({ course: course });
+  }
+
+  onClickSave() {
+    alert(`Saving ${this.state.course.title}`);
+  }
+
   render() {
     return (
       <div>
         <h1>Courses</h1>
+        <h2>Add course</h2>
+        <input
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title} />
+
+        <input
+          type="submit"
+          value="Save"
+          onClick={this.onClickSave} />
       </div>
     );
   }
