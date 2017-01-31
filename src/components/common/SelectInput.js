d5a292562fa8e41b4224c0aecbc5566f69e8ce5d
Nate Dean <nate.dean@pathar.net>
2017-01-31 16:47:38 -0600

    More done...

---
+++ src/components/common/SelectInput.js
@@ -0,0 +1,21 @@
+import React, { PropTypes } from 'react';
+
+const SelectInput = ({name, label, onChange, defaultOption, value, error, options}) => {
+  return (
+    <div className="form-group">
+      <label htmlFor={name}>{label}</label>
+      <div className="field">
+        <select
+          name={name}
+          value={value}
+          onChange={onChange}
+          className="form-control">
+          <option value="">{defaultOption}</option>
+          {options.map(option => {
+            return <option key={option.value} value={option.value}>{option.text}</option>;
+          })}
+        </select>
+      </div>
+    </div>
+  );
+};
